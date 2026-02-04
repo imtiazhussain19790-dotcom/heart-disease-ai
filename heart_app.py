999b41ea7fe3d08847c1d56ebea181cc1b0e1170
imtiazhussain19790-dotcom <imtiazhussain19790@gmail.com>
2026-02-04 10:06:50 +0500

    Update heart_app.py

--- heart_app.py
+++ heart_app.py
@@ -1,8 +1,24 @@
 import streamlit as st
 import joblib
 import numpy as np 
+from fpdf import FPDF
 model=joblib.load('heart_model.pkl')
+def create_pdf(age, sex, prediction):
+    pdf = FPDF()
+    pdf.add_page()
+    pdf.set_font("Arial", size=16)
+    pdf.cell(200, 10, txt="Heart Disease Diagnostic Report", ln=True, align='C')
+    pdf.set_font("Arial", size=12)
+    pdf.cell(200, 10, txt=f"Patient Age: {age}", ln=True)
+    pdf.cell(200, 10, txt=f"Gender: {'Male' if sex==1 else 'Female'}", ln=True)
+    
+    result_text = "Positive (Risk Detected)" if prediction == 1 else "Negative (Normal)"
+    pdf.cell(200, 10, txt=f"Final Result: {result_text}", ln=True)
+    
+    return pdf.output(dest='S').encode('latin-1')
 st.title('Heart Disease Prediction System')
+age = st.number_input("Age", min_value=1, max_value=120, value=54)
+sex = st.selectbox("Sex", options=[1, 0], format_func=lambda x: 'Male' if x==1 else 'Female')
 st.write("Enter the patient's health metrics below to predict the risk of heart disease")
 st.sidebar.header("Patient Data Input")
 age = st.number_input("Age (in years)", min_value=1, max_value=120, value=54)
@@ -16,4 +32,9 @@ if st.button("Predict"):
         if prediction [0]==1:
                 st.error("Warning:The Patient is likely to have heart disease. please consult the doctor.")
         else:
-                st.success("Result: The patient is unlikely to have heart disease.")       
+                st.success("Result: The patient is unlikely to have heart disease.")
+        pdf_bytes = create_pdf(age, sex, prediction[0])
+        st.download_button(label="📥 Download Report (PDF)", 
+data=pdf_bytes, 
+file_name="heart_report.pdf", 
+mime="application/pdf")
